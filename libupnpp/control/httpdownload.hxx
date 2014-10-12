c2fa9f72652b4e292e6ac009d89a765ade8504c9
Jean-Francois Dockes <jf@dockes.org>
2014-10-12 15:55:20 +0200

    separated libupnpp

--- libupnpp/control/httpdownload.hxx
+++
@@ -1,9 +0,0 @@
-#ifndef _HTTPDOWNLOAD_H_X_INCLUDED_
-#define _HTTPDOWNLOAD_H_X_INCLUDED_
-
-#include <string>
-
-extern bool downloadUrlWithCurl(const std::string& url, 
-                                std::string& out, long timeoutsecs);
-
-#endif /* _HTTPDOWNLOAD.H_X_INCLUDED_ */
