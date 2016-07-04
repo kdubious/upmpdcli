5351a6036b4a36da9b99b33f7fd5dd3be3bb5778
Jean-Francois Dockes <jf@dockes.org>
2016-07-04 10:43:48 +0200

    removed pre-c++11 compat config

--- src/conf_post.h
+++ src/conf_post.h
@@ -26,6 +26,9 @@
 #define UPMPDCLI_PACKAGE_VERSION_DEFINED
 static const char *UPMPDCLI_PACKAGE_VERSION = PACKAGE_VERSION;
 #endif
+
+#define UPMPDCLI_SOURCE
+
 #undef PACKAGE_BUGREPORT
 #undef PACKAGE_NAME
 #undef PACKAGE_STRING
@@ -34,46 +37,14 @@ static const char *UPMPDCLI_PACKAGE_VERSION = PACKAGE_VERSION;
 #undef PACKAGE_VERSION
 
 #ifdef __cplusplus
-#ifdef  HAVE_CXX0X_UNORDERED
-#  define UNORDERED_MAP_INCLUDE <unordered_map>
-#  define UNORDERED_SET_INCLUDE <unordered_set>
-#  include <functional>
-#  define STD_UNORDERED_MAP std::unordered_map
-#  define STD_UNORDERED_SET std::unordered_set
-#  define STD_FUNCTION      std::function
-#  define STD_BIND          std::bind
-#  define STD_PLACEHOLDERS  std::placeholders
-#elif defined(HAVE_TR1_UNORDERED)
-#  define UNORDERED_MAP_INCLUDE <tr1/unordered_map>
-#  define UNORDERED_SET_INCLUDE <tr1/unordered_set>
-#  include <tr1/functional>
-#  define STD_UNORDERED_MAP std::tr1::unordered_map
-#  define STD_UNORDERED_SET std::tr1::unordered_set
-#  define STD_FUNCTION      std::tr1::function
-#  define STD_BIND          std::tr1::bind
-#  define STD_PLACEHOLDERS  std::tr1::placeholders
-#else
-#  define UNORDERED_MAP_INCLUDE <map>
-#  define UNORDERED_SET_INCLUDE <set>
-#  define STD_UNORDERED_MAP std::map
-#  define STD_UNORDERED_SET std::set
-/* Yeah we're cooked if the code uses these features */
-#  define STD_FUNCTION 
-#  define STD_BIND
-#  define STD_PLACEHOLDERS
-#endif
-
-#ifdef HAVE_SHARED_PTR_STD
-#  define MEMORY_INCLUDE <memory>
-#  define STD_SHARED_PTR    std::shared_ptr
-#  define STD_WEAK_PTR      std::weak_ptr
-#elif defined(HAVE_SHARED_PTR_TR1)
-#  define MEMORY_INCLUDE <tr1/memory>
-#  define STD_SHARED_PTR    std::tr1::shared_ptr
-#  define STD_WEAK_PTR      std::tr1::weak_ptr
-#else
-#  define MEMORY_INCLUDE "refcntr.h"
-#  define STD_SHARED_PTR    RefCntr
-#endif
-
+#define UNORDERED_MAP_INCLUDE <unordered_map>
+#define UNORDERED_SET_INCLUDE <unordered_set>
+#define STD_UNORDERED_MAP std::unordered_map
+#define STD_UNORDERED_SET std::unordered_set
+#define STD_FUNCTION      std::function
+#define STD_BIND          std::bind
+#define STD_PLACEHOLDERS  std::placeholders
+#define MEMORY_INCLUDE <memory>
+#define STD_SHARED_PTR    std::shared_ptr
+#define STD_WEAK_PTR      std::weak_ptr
 #endif /* c++ */
